8895c3b331329fe253ee336a78084e0f41dd05c0
jtwyt <1400012901@pku.edu.cn>
2016-12-08 11:31:40 +0800

    Update tcpheader.hh

--- local/tcpheader.hh
+++ local/tcpheader.hh
@@ -9,13 +9,6 @@ struct tcpheader
 	unsigned Offset: 8;
 	unsigned Flag: 8;
 	unsigned Checksum: 16;
-	void initialize()
-	{
-		SeqNum = AckNum = 0;
-		Offset = 0;
-		Flag = 0;
-		Checksum = 0;
-	}
 };
 
 #endif
